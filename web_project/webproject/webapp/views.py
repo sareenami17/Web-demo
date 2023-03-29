0e08c4eff629a65ed8a06cf930ec41a7e12c1c30
sareenami17 <sareenami17gmail.com>
2023-03-29 13:33:39 +0530

    ADDED

---
+++ web_project/webproject/webapp/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from . models import Place,Team
+from django.http import HttpResponse
+
+# Create your views here.
+
+
+
+def demo(request):
+    obj=Place.objects.all()
+    obj1 = Team.objects.all()
+    return render(request,"index.html",{'result':obj,"res":obj1})
